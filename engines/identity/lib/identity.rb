e9b4c1721ff98f5e5e946304ea66e1eb688fb3bb
Paul Caplan <pcaplan@teladoc.com>
2025-12-27 21:31:30 -0500

    rails plugin new engines/identity --mountable

---
+++ engines/identity/lib/identity.rb
@@ -0,0 +1,6 @@
+require "identity/version"
+require "identity/engine"
+
+module Identity
+  # Your code goes here...
+end
---
+++ engines/identity/test/identity_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class IdentityTest < ActiveSupport::TestCase
+  test "it has a version number" do
+    assert Identity::VERSION
+  end
+end
